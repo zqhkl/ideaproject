87c0434e1232ed11331cf049795328744dc79e3e
Zhangqinghua <asd6857702@163.com>
2024-02-02 20:52:39 +0800

    first

---
+++ spring6/spring6-002-first/src/main/java/com/powernode/spring6/dao/UserDaoImplForMySQL.java
@@ -0,0 +1,15 @@
+package com.powernode.spring6.dao;
+
+/**
+ * bean
+ * @author 动力节点
+ * @version 1.0
+ * @className UserDaoImplForMySQL
+ * @since 1.0
+ **/
+public class UserDaoImplForMySQL {
+
+    public void insert(){
+        System.out.println("mysql正在保存用户信息");
+    }
+}
